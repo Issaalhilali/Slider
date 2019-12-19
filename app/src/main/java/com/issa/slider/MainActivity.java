6425ec0ff527f53391f933ea782d82e8acd5bf3f
issa12345 <issaalhiali@gmail.com>
2019-12-19 22:51:14 +0300

    Initial commit

---
+++ app/src/main/java/com/issa/slider/MainActivity.java
@@ -0,0 +1,150 @@
+package com.issa.slider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
+    RequestQueue rq;
+    List<SliderUtils> sliderImg;
+    String request_url = "https://gist.githubusercontent.com/Issaalhilali/a6d1dfa65a9ea25a3f1b45c435d7fb87/raw/148fa6a1ac731feed61ac71cd60d0abb203bcb27/Sliderissa";
+    ViewPagerAdapter viewPagerAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rq = Volley.newRequestQueue(this);
+        sliderImg = new ArrayList<>();
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        sliderDotspanel = (LinearLayout) findViewById(R.id.SliderDots);
+
+        sendRequest();
+
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 2000, 4000);
+
+    }
+
+
+    public class MyTimerTask extends TimerTask{
+
+        @Override
+        public void run() {
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if(viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1);
+                    } else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+
+                }
+            });
+        }
+    }
+
+    public void  sendRequest(){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, request_url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for (int i = 0 ; i < response.length(); i ++){
+                    SliderUtils sliderUtils = new SliderUtils();
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        sliderUtils.setSliderImageUrl(jsonObject.getString("image_url"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    sliderImg.add(sliderUtils);
+                }
+
+                viewPagerAdapter = new ViewPagerAdapter(sliderImg, MainActivity.this);
+
+                viewPager.setAdapter(viewPagerAdapter);
+
+                dotscount = viewPagerAdapter.getCount();
+                dots = new ImageView[dotscount];
+
+                for(int i = 0; i < dotscount; i++){
+
+                    dots[i] = new ImageView(MainActivity.this);
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                    params.setMargins(8, 0, 8, 0);
+
+                    sliderDotspanel.addView(dots[i], params);
+
+                }
+
+                dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        rq.add(jsonArrayRequest);
+    }
+}
---
+++ app/src/main/java/com/issa/slider/SliderUtils.java
@@ -0,0 +1,18 @@
+package com.issa.slider;
+
+public class SliderUtils {
+
+    String sliderImageUrl;
+
+    public SliderUtils() {
+        this.sliderImageUrl = sliderImageUrl;
+    }
+
+    public String getSliderImageUrl() {
+        return sliderImageUrl;
+    }
+
+    public void setSliderImageUrl(String sliderImageUrl) {
+        this.sliderImageUrl = sliderImageUrl;
+    }
+}
