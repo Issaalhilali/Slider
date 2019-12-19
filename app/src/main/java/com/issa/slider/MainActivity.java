597cb76d40d67478f7431f91b41c891899b4ee67
issa12345 <issaalhiali@gmail.com>
2019-12-19 23:28:38 +0300

    update

--- app/src/main/java/com/issa/slider/MainActivity.java
+++ app/src/main/java/com/issa/slider/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView[] dots;
     RequestQueue rq;
     List<SliderUtils> sliderImg;
-    String request_url = "https://gist.githubusercontent.com/Issaalhilali/a6d1dfa65a9ea25a3f1b45c435d7fb87/raw/148fa6a1ac731feed61ac71cd60d0abb203bcb27/Sliderissa";
+    String request_url = "https://gist.githubusercontent.com/Issaalhilali/a6d1dfa65a9ea25a3f1b45c435d7fb87/raw/7101017c65201cad2b66c814ef5206884cf48cb5/Sliderissa";
     ViewPagerAdapter viewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,6 +91,16 @@ public class MainActivity extends AppCompatActivity {
                         viewPager.setCurrentItem(1);
                     } else if(viewPager.getCurrentItem() == 1){
                         viewPager.setCurrentItem(2);
+                    }  else if(viewPager.getCurrentItem() == 2) {
+                        viewPager.setCurrentItem(3);
+                    }else if(viewPager.getCurrentItem() == 3){
+                        viewPager.setCurrentItem(4);
+                    } else if(viewPager.getCurrentItem() == 4){
+                        viewPager.setCurrentItem(5);
+                    } else if(viewPager.getCurrentItem() == 5){
+                        viewPager.setCurrentItem(6);
+                    } else if(viewPager.getCurrentItem() == 6){
+                        viewPager.setCurrentItem(7);
                     } else {
                         viewPager.setCurrentItem(0);
                     }
